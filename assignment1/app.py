a49914558b80c8e44216098ad7a2d52b4b76b407
skaneplug <rainnyforeverluv@gmail.com>
2017-10-15 11:00:00 -0700

    create flask app

--- assignment1/app.py
+++ assignment1/app.py
@@ -1,23 +1,27 @@
 import subprocess
-from flask import Flask, redirect, url_for, request, jsonify
-import sqlite3
+from flask import Flask, request, jsonify
+import rocksdb
 import os
-import socket
+
 
 #File Upload
-UPLOAD_FOLDER = 'uploads_py'
+UPLOAD_FOLDER = './uploads_py'
 ALLOWED_EXTENSIONS = set(['py'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-SCID = 0
-#DB Connection
+db = rocksdb.DB("assign1.db", rocksdb.Options(create_if_missing=True))
 
 def get_scriptId():
-    global SCID
-    SCID += 1
-    return SCID
+    #global SCID
+    global db
+    output = db.get(bytes("scid", "ascii"))
+    if output is None:
+        output = bytes("0", "ascii")
+    scid = int(output.decode("ascii")) + 1
+    db.put(bytes("scid", "ascii"), bytes(str(scid), "ascii"))
+    return scid
 
 
 def allowed_file(filename):
@@ -37,7 +41,7 @@ def run_script(id):
 def index():
     return 'welcome'
 
-@app.route('/api/v1/scripts', methods=['GET', 'POST'])
+@app.route('/api/v1/scripts', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['data']
@@ -47,19 +51,14 @@ def upload_file():
         if file and allowed_file(file.filename):
             script_id = get_scriptId()
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], str(script_id)+".py"))
-            return jsonify({"script-id": script_id})
+            return jsonify({"script-id": script_id}), 201
 
 @app.route('/api/v1/scripts/<id>')
 def render2(id):
     get_scriptId()
     return run_script(id)
 
-@app.route('/hello')
-def hello():
-    html = "<h3>Hello? {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
